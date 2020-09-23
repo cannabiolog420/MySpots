c30427b9dd8a6051b33e02ff789ca3fb4abc9ba2
cannabiolog420 <darkstar125@mail.ru>
2020-09-23 17:04:05 +0300

    Initial Commit

---
+++ MySpots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySpots
+//
+//  Created by cannabiolog420 on 23.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
